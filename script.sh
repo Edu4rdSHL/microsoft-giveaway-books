f9e794fcd285e8be06e29d9f525ff15f8efb095e
Eduard Tolosa <tolosaeduard@gmail.com>
2019-08-03 20:42:19 -0500

    Initial commit

---
+++ script.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+VERSION=0.1
+echo "Microsoft GIVE AWAY files downloader. Version $VERSION"
+echo
+echo "Star the project and follow @edu4rdshl in Github/Twitter, it will be appreciated."
+echo "Downloading books links..."
+if wget -q -O "links-temp.txt" "https://msdnshared.blob.core.windows.net/media/2017/07/Ligman_eBooks_2017.txt"; then
+  echo "Books links correctly downloaded."
+  tail -n +2 "links-temp.txt" > "links.txt"
+  sed -i -r 's/\s+//g' "links.txt"
+  rm -f "links-temp.txt"
+  TOTAL_BOOKS=$(wc -l "links.txt" | awk '{print $1;}')
+  echo "Total books to download: $TOTAL_BOOKS"
+  echo "Downloading books now, do not interrupt the process..."
+  COUNTER=0;
+  while read LINK; do
+    if wget -q --content-disposition $LINK; then
+      COUNTER=$[$COUNTER + 1];
+      echo "Sucessfully downloaded book number: $COUNTER"
+    else
+      COUNTER=$[$COUNTER + 1];
+      echo "A error as ocurred while downloading the book: $COUNTER"
+    fi
+  done < links.txt
+else
+  echo "Error downloading books links, exiting."
+  exit
+fi
